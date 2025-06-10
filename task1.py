2261e442e2e47462b7dc01262f361c4af0ba8f6e
AlexRook777 <alexrook777@gmail.com>
2025-06-10 09:22:16 +0300

    task 1 done

--- task1.py
+++ task1.py
@@ -1,1 +1,14 @@
-111
+from datetime import datetime
+
+def get_days_from_today(date):
+    try:
+        input_date = datetime.strptime(date, "%Y-%m-%d")
+        today = datetime.today()
+        delta = (today - input_date).days
+    except:
+        delta = None
+    return delta
+
+input_date = "2025-06-30"
+days_difference = get_days_from_today(input_date)   
+print(f"Days from today to {input_date}: {days_difference}") if days_difference is not None else print("Invalid date format")
